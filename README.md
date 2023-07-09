79736eb94a9ebfcfcf08708672f14337c237c5b1
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-07-09 20:07:57 +0700

    feat(boostrap): auto create ansible vpss

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# ansible-web-app-deployment
+<h1 align="center">
+  <img src="./assets/ansible.png" alt="icon" width="200"></img>
+  <br>
+  <b>Ansible VPS Generator</b>
+</h1>
+
+<p align="center">Helping you auto create Ansible VPS and Ubuntu VPSs, connect them and auto generate Inventory file</p>
+
+<!-- Badges -->
+<p align="center">
+  <a href="https://github.com/QuanBlue/ansible-vps-generator/graphs/contributors">
+    <img src="https://img.shields.io/github/contributors/QuanBlue/ansible-vps-generator" alt="contributors" />
+  </a>
+  <a href="">
+    <img src="https://img.shields.io/github/last-commit/QuanBlue/ansible-vps-generator" alt="last update" />
+  </a>
+  <a href="https://github.com/QuanBlue/ansible-vps-generator/network/members">
+    <img src="https://img.shields.io/github/forks/QuanBlue/ansible-vps-generator" alt="forks" />
+  </a>
+  <a href="https://github.com/QuanBlue/ansible-vps-generator/stargazers">
+    <img src="https://img.shields.io/github/stars/QuanBlue/ansible-vps-generator" alt="stars" />
+  </a>
+  <a href="https://github.com/QuanBlue/ansible-vps-generator/issues/">
+    <img src="https://img.shields.io/github/issues/QuanBlue/ansible-vps-generator" alt="open issues" />
+  </a>
+  <a href="https://github.com/QuanBlue/ansible-vps-generator/blob/main/LICENSE">
+    <img src="https://img.shields.io/github/license/QuanBlue/ansible-vps-generator.svg" alt="license" />
+  </a>
+</p>
+
+<p align="center">
+  <b>
+    <a href="https://github.com/QuanBlue/ansible-vps-generator">Documentation</a> •
+    <a href="https://github.com/QuanBlue/ansible-vps-generator/issues/">Report Bug</a> •
+    <a href="https://github.com/QuanBlue/ansible-vps-generator/issues/">Request Feature</a>
+  </b>
+</p>
+<br/>
+<details open>
+<summary><b>Table of Contents</b></summary>
+
+-  [:toolbox: Getting Started](#toolbox-getting-started)
+   -  [Prerequisites](#prerequisites)
+   -  [Environment Variables](#environment-variables)
+-  [:rocket: Usage](#rocket-usage)
+-  [:world_map: Roadmap](#world_map-roadmap)
+-  [:busts_in_silhouette: Contributors](#busts_in_silhouette-contributors)
+-  [:sparkles: Credits](#sparkles-credits)
+-  [:scroll: License](#scroll-license)
+</details>
+
+# :toolbox: Getting Started
+
+## Prerequisites
+
+Before proceeding with the installation and usage of this project, ensure that you have the following prerequisites in place:
+
+-  **Docker Engine:** Docker provides a consistent and portable environment for running applications in containers. Install [here](https://www.docker.com/get-started/).
+-  **Network Connectivity:** Docker requires network connectivity to download images, communicate with containers, and access external resources.
+
+## Environment Variables
+
+# :rocket: Usage
+
+# :world_map: Roadmap
+
+# :busts_in_silhouette: Contributors
+
+<a href="https://github.com/QuanBlue/Linux-Bootstrap/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=QuanBlue/Linux-Bootstrap" />
+</a>
+
+Contributions are always welcome!
+
+# :sparkles: Credits
+
+-  [Docker](https://www.docker.com/)
+-  [Ansible](https://www.ansible.com/)
+-  Emojis are taken from [here](https://github.com/arvida/emoji-cheat-sheet.com)
+
+# :scroll: License
+
+Distributed under the MIT License. See <a href="../LICENSE">`LICENSE`</a> for more information.
+
+---
+
+> Bento [@quanblue](https://bento.me/quanblue) &nbsp;&middot;&nbsp;
+> GitHub [@QuanBlue](https://github.com/QuanBlue) &nbsp;&middot;&nbsp; Gmail quannguyenthanh558@gmail.com
---
+++ bootstrap.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Load environment variables
+source .env
+
+NETWORK=${NETWORK:-ansible-net}
+REMOTE_MACHINE=(grafana prometheus node_exporter alert_manager)
+INVENTORY="./ansible/inventories/hosts.ini"
+CONTROLLER_PATH_TO_INVNETORY="/etc/ansible/inventories/hosts.ini"
+
+# Function to print messages in pretty format
+info() {
+   if [ "$1" = "header" ]; then
+      tput bold       # Sets the text to bold
+      tput setaf 7    # Sets the text color to white (ANSI color code 7)
+      tput setab 2    # Sets the background color to green (ANSI color code 2)
+      printf "\n$2\n" # Prints the message passed as an argument to the function
+      tput sgr0       # Resets text attributes (color, boldness, etc.) to default
+      tput el
+   else
+      tput bold    # Sets the text to bold
+      tput setaf 2 # Sets the text color to green (ANSI color code 2)
+      printf "$1"  # Prints the message passed as an argument to the function without new line
+      tput sgr0    # Resets text attributes (color, boldness, etc.) to default
+      tput el
+   fi
+}
+
+# Function to split string
+split_string() {
+   local input_string="$1"    # Input string
+   local delimiter="$2"       # Delimiter character or string
+   local -n output_array="$3" # Output array variable (passed by reference)
+
+   IFS="$delimiter" read -r -a output_array <<<"$input_string"
+}
+
+# Remove old containers
+info header "Remove old containers"
+info "Removing..."
+echo
+docker rm -f ansible-controller
+for machine in ${REMOTE_MACHINE[@]}; do
+   docker rm -f $machine
+done
+info "Complete!"
+
+# Create network
+info header "Create network '${NETWORK}'"
+info "Creating..."
+docker network create --driver=bridge ${NETWORK}
+info "Complete!"
+
+# Create Ansible container
+info header "Create Ansible controller container"
+info "Creating..."
+docker build -t ansible-controller:v1 -f ./dockerfile/Dockerfile-ansible-machine .
+docker run -itd \
+   --name ansible-controller \
+   --network ${NETWORK} \
+   --volume ./ansible/:/etc/ansible/ \
+   ansible-controller:v1
+rsa_pub=$(docker exec -it ansible-controller sh -c 'cat ~/.ssh/id_rsa.pub')
+info "Complete!"
+
+# Build REMOTE MACHINE container
+info header "Create Remote Machine container"
+info "Building image..."
+docker build -t ubuntu-node:18.04 -f ./dockerfile/Dockerfile-remote-machine .
+info "Complete!"
+
+# Create REMOTE MACHINE container
+for machine in ${REMOTE_MACHINE[@]}; do
+   info "Creating ${machine} VPS... "
+
+   docker run -itd \
+      --name=${machine} \
+      --network=${NETWORK} \
+      --hostname=${machine} \
+      ubuntu-node:18.04
+
+   docker exec -it ${machine} sh -c "mkdir -p /root/.ssh && echo '${rsa_pub}' > /root/.ssh/authorized_keys"
+
+   info "Complete!"
+
+done
+info "Complete!"
+
+# Get IP address of containers
+info header "Get IP address of containers"
+info "Getting..."
+delimiter=";"
+ipv4=$(docker network inspect --format="{{range .Containers}}{{.Name}}:{{.IPv4Address}}{{\"$delimiter\"}}{{end}}" $NETWORK)
+split_string "$ipv4" "$delimiter" ipv4_array
+info "Complete!"
+
+# Generate inventory content
+info header "Generate inventory content"
+info "Generating..."
+inventory=""
+for element in "${ipv4_array[@]}"; do
+   split_string "$element" ":" machine_props
+   split_string "${machine_props[1]}" "/" ip
+
+   machine_name=${machine_props[0]}
+   machine_ip=${ip[0]}
+
+   inventory+="[${machine_name}]"
+   inventory+="\n${machine_name}_host ansible_host=${machine_ip}\n\n"
+
+   if [ ${machine_name} != "ansible" ]; then
+      # check connection from Controller to Nodes
+      info "Checking connection from Controller to ${machine_name}..."
+      printf "exit\n" | docker exec -i ansible-controller sh -c "ssh -o 'StrictHostKeyChecking=no' root@${machine_ip}"
+      info "Complete!"
+   fi
+done
+info "Complete!"
+
+# Write inventory file
+info header "Write inventory file"
+info "Writing..."
+echo -e $inventory >$INVENTORY
+info "Complete!"
+
+# Check Inventory file
+info header "List all hosts Inventory file"
+info "Listing..."
+docker exec -it ansible-controller sh -c "ansible-inventory -i $CONTROLLER_PATH_TO_INVNETORY --list"
+info "Complete!"
